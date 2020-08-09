a01be63066c129f813eefb31d6d40d38cc21ee39
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-08-10 00:15:30 +0300

    Updates

---
+++ precache-manifest.66624aa0ab64ed2bfd12fe46a5b4461c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61c8b0c5f7766ffa338699df4276dd2",
+    "url": "/calc-page/index.html"
+  },
+  {
+    "revision": "a2c1630d539d8be8700b",
+    "url": "/calc-page/static/css/main.2d8fa879.chunk.css"
+  },
+  {
+    "revision": "c6714b043608c32b3b1f",
+    "url": "/calc-page/static/js/2.a5efecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc-page/static/js/2.a5efecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c1630d539d8be8700b",
+    "url": "/calc-page/static/js/main.97dcc9bc.chunk.js"
+  },
+  {
+    "revision": "a3541225d3aefad03aeb",
+    "url": "/calc-page/static/js/runtime-main.86a44ee1.js"
+  }
+]);
